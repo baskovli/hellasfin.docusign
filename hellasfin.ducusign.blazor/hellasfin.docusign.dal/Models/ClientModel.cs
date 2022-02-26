65cd511d8b52222341b64462111d557dea90a34e
Mel Ado <melikalai232@gmail.com>
2022-02-26 19:10:25 +0200

    added grid

---
+++ hellasfin.ducusign.blazor/hellasfin.docusign.dal/Models/ClientModel.cs
@@ -0,0 +1,12 @@
+﻿namespace hellasfin.docusign.dal.Models
+{
+    public class ClientModel
+    {
+        public int Id { get; set; }
+        public string Company { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+    }
+}
