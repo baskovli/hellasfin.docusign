6f36e4ab4092bdb5364cf85e256bd254880c2e8f
Mel Ado <melikalai232@gmail.com>
2022-03-12 12:23:20 +0200

    starting auth docusign

---
+++ hellasfin.ducusign.blazor/hellasfin.ducusign.blazor/Helpers/StartUpHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace hellasfin.ducusign.blazor.Helpers
+{
+    internal static class StartUpHelper
+    {
+        public static string ExtractDefaultAccountValue(JsonElement obj, string key)
+        {
+            if (!obj.TryGetProperty("accounts", out var accounts))
+            {
+                return null;
+            }
+
+            string keyValue = null;
+
+            foreach (var account in accounts.EnumerateArray())
+            {
+                if (account.TryGetProperty("is_default", out var defaultAccount) && defaultAccount.GetBoolean())
+                {
+                    if (account.TryGetProperty(key, out var value))
+                    {
+                        keyValue = value.GetString();
+                    }
+                }
+            }
+
+            return keyValue;
+        }
+    }
+}
--- hellasfin.ducusign.blazor/hellasfin.ducusign.blazor/Program.cs
+++ hellasfin.ducusign.blazor/hellasfin.ducusign.blazor/Program.cs
@@ -4,7 +4,16 @@ using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
 using hellasfin.ducusign.blazor.Data;
+using hellasfin.ducusign.blazor.Esignature;
+using hellasfin.ducusign.blazor.Helpers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Newtonsoft.Json.Linq;
 using System.Globalization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,6 +39,73 @@ builder.Services.AddBlazorise(options =>
 builder.Services.AddBlazoredToast();
 builder.Services.AddBlazoredModal();
 
+
+DSConfiguration config = new DSConfiguration();
+
+builder.Configuration.Bind("DocuSign", config);
+
+builder.Services.AddSingleton(config);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = "DocuSign";
+})
+            .AddCookie()
+            .AddOAuth("DocuSign", options =>
+            {
+                options.ClientId = builder.Configuration["DocuSign:ClientId"];
+                options.ClientSecret = builder.Configuration["DocuSign:ClientSecret"];
+                options.CallbackPath = new PathString("/ds/callback");
+
+                options.AuthorizationEndpoint = builder.Configuration["DocuSign:AuthorizationEndpoint"];
+                options.TokenEndpoint = builder.Configuration["DocuSign:TokenEndpoint"];
+                options.UserInformationEndpoint = builder.Configuration["DocuSign:UserInformationEndpoint"];
+                options.Scope.Add("signature");                
+
+                options.SaveTokens = true;
+                options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
+                options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                options.ClaimActions.MapJsonKey("accounts", "accounts");
+
+                options.ClaimActions.MapCustomJson("account_id", obj => StartUpHelper.ExtractDefaultAccountValue(obj, "account_id"));
+                options.ClaimActions.MapCustomJson("account_name", obj => StartUpHelper.ExtractDefaultAccountValue(obj, "account_name"));
+                options.ClaimActions.MapCustomJson("base_uri", obj => StartUpHelper.ExtractDefaultAccountValue(obj, "base_uri"));
+                options.ClaimActions.MapJsonKey("access_token", "access_token");
+                options.ClaimActions.MapJsonKey("refresh_token", "refresh_token");
+                options.ClaimActions.MapJsonKey("expires_in", "expires_in");
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = async context =>
+                    {
+                        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                        var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+                        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                        user.Add("access_token", context.AccessToken);
+                        user.Add("refresh_token", context.RefreshToken);
+                        user.Add("expires_in", DateTime.Now.Add(context.ExpiresIn.Value).ToString());
+
+                        using (JsonDocument payload = JsonDocument.Parse(user.ToString()))
+                        {
+                            context.RunClaimActions(payload.RootElement);
+                        }
+                    },
+                    OnRemoteFailure = context =>
+                    {
+                        context.HandleResponse();
+                        context.Response.Redirect("/Home/Error?message=" + context.Failure.Message);
+                        return Task.FromResult(0);
+                    }
+                };
+            });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,13 +115,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+ app.UseAuthentication();
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthorization();
+app.UseAuthentication();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
